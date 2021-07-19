a469a4904ce07d2d39c68e06445d964a0be19b64
joejaeyoung <68009715+joejaeyoung@users.noreply.github.com>
2021-07-19 13:44:09 +0900

    Add files via upload
    백준 단계별로 풀어보기

---
+++ ARR1_4344.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ARR1_4344 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int totalTestNum = in.nextInt();
+
+        int[] inputScore;
+
+        for(int i=0; i<totalTestNum; i++){
+            int studentNum = in.nextInt();
+            int sum=0;
+            double average=0;
+            int highStudentNum=0;
+
+            inputScore = new int[studentNum];
+            for(int j=0; j<studentNum; j++){
+                int val = in.nextInt();
+                inputScore[j] = val;
+                sum+=val;
+            }
+            average = (double)sum/studentNum;
+
+            for(int j=0; j<studentNum; j++){
+                if(inputScore[j]>average)
+                    highStudentNum++;
+            }
+
+            System.out.printf("%.3%%lf",(double)(studentNum/highStudentNum)*100);
+
+        }
+        in.close();
+    }
+}
